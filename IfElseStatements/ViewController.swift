b9e66785bb73bef167218603126b2ed66cd604c0
Keanna <keannakr@hawaii.edu>
2019-01-31 10:30:41 -1000

    Initial Commit

---
+++ IfElseStatements/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IfElseStatements
+//
+//  Created by Keanna Ramiscal on 1/31/19.
+//  Copyright © 2019 Keanna Ramiscal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
